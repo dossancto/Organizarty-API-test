bc671afd5c184799be6a46b7cb7adca17d6089dd
lu-css <dossantossilvalucas963@gmail.com>
2023-09-29 20:07:59 -0300

    refactor: move infra providers to extension

--- Organizarty.Application/Program.cs
+++ Organizarty.Application/Program.cs
@@ -1,6 +1,3 @@
-using Organizarty.Adapters;
-using Organizarty.Infra.Providers.Cryptography;
-using Organizarty.Infra.Providers.Token;
 using Organizarty.Infra.Extensions;
 using Organizarty.Domain.UseCases.Users;
 using Organizarty.Application.Services;
@@ -10,15 +7,13 @@ Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddDatabase(builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// TODO: Move to extention
-builder.Services.AddScoped<ICryptographys, Pbkdf2>();
-builder.Services.AddScoped<ITokenAdapter, JWT>();
+builder.Services.AddProviders();
 
 builder.Services.AddScoped<ISignUseCase, SignService>();
 
--- Organizarty.Infra/Extensions/InfraExtention.cs
+++ Organizarty.Infra/Extensions/InfraExtention.cs
@@ -1,7 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Organizarty.Adapters;
 using Organizarty.Infra.Data.Contexts;
+using Organizarty.Infra.Providers.Cryptography;
+using Organizarty.Infra.Providers.Token;
 
 namespace Organizarty.Infra.Extensions;
 
@@ -24,12 +27,20 @@ public static class InfraExtention
         return configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connect string not found");
     }
 
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<ApplicationDbContext>(options =>
           options.UseMySql(GetConnectionString(configuration),
           new MySqlServerVersion(new Version(8, 0, 26)),
           b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+        return services;
+    }
+
+    public static IServiceCollection AddProviders(this IServiceCollection services)
+    {
+        services.AddScoped<ICryptographys, Pbkdf2>();
+        services.AddScoped<ITokenAdapter, JWT>();
 
         return services;
     }
